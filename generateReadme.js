7bd9a22a42d3e38d725f20c891243db0ae92db13
Eric Willhoit <ewillhoit@salesforce.com>
2022-11-22 14:46:30 -0600

    chore: remove circle

--- generateReadme.js
+++ generateReadme.js
@@ -11,23 +11,25 @@ let contents = `${HEADER}\n`;
 
 const list = repositories;
 
-contents += '| Repository | Package | Released | Status |\n';
-contents += '|------------|---------|----------|--------|\n';
+contents += '| Repository | Type | Package | Released |\n';
+contents += '|------------|:----:|---------|----------|\n';
 
 /**
- * Get the img.shield badge as <type><name><version>.
- * @param {name: string, type: 'package' | 'library' | 'orb'} package
+ * Get the img.shield badge as <type>.
+ * @param {name: string, type: 'package' | 'library' } package
+ */
+function getTypeBadge(package) {
+  const color = package.type === 'plugin' ? 'blue' : package.type === 'library' ? 'yellowgreen' : 'orange';
+
+  return `![type](https://img.shields.io/badge/%20-${package.type}-${color})`;
+}
+
+/**
+ * Get the img.shield badge as <name><version>.
+ * @param {name: string, type: 'package' | 'library' } package
  */
 function getPackageBadge(package) {
-  let row = ''
-  const color = package.type === 'plugin' ? 'blue' : package.type === 'library' ? 'yellowgreen' : 'orange';
-  row += `![type](https://img.shields.io/badge/%20-${package.type}-${color})`;
-  if (package.type === 'orb') {
-    row += `[![CircleCI Orb Version](https://img.shields.io/badge/endpoint.svg?label=${package.name}&url=https://badges.circleci.io/orb/${package.name})](https://circleci.com/orbs/registry/orb/${package.name})`
-  } else {
-    row += `[![NPM](https://img.shields.io/npm/v/${package.name}.svg?label=${package.name})](https://www.npmjs.com/package/${package.name})`;
-  }
-  return row;
+  return `[![NPM](https://img.shields.io/npm/v/${package.name}.svg?label=${package.name})](https://www.npmjs.com/package/${package.name})`;
 }
 
 for (const repo of list) {
@@ -38,11 +40,11 @@ for (const repo of list) {
   const supportsReleases = typeof repo.supportsReleases === 'boolean' ? repo.supportsReleases : true;
 
   const repository = `[${slug}](https://github.com/${slug})`;
+  const type = repo.packages.map(getTypeBadge).join('<br>');
   const packages = repo.packages.map(getPackageBadge).join('<br>');
   const released = !repo.private && supportsReleases ? `![GitHub Release Date](https://img.shields.io/github/release-date/${slug}?color=ffc16b&label=%20)` : '';
-  const status = !repo.private ? `[![circleci](https://circleci.com/gh/${slug}.svg?style=svg)](https://app.circleci.com/pipelines/github/${slug})` : '';
 
-  const tableSegments = [ repository, packages, released, status ]
+  const tableSegments = [repository, type, packages, released];
   contents += `| ${tableSegments.join(' | ')} |\n`;
 }
 
