5fd7420f53f3d6d554c23ea75d2678ea9460e73e
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2024-04-25 14:46:04 -0500

    feat: update repo list (#49)
    * chore: maintain repo list
    
    * chore: add source-testkit

--- generateReadme.js
+++ generateReadme.js
@@ -1,26 +1,46 @@
 const fs = require('fs');
 
 const repositories = require('./repositories.json');
-
 const readme = fs.readFileSync('./README.md', 'utf8');
-
 const HEADER = '<!-- Repositories -->\n';
 const repositoryIndex = readme.indexOf(HEADER);
 
-let contents = `${HEADER}\n`;
 
-const list = repositories;
+const repoToTableRow = (repo) => {
+  const url = repo.url;
+  // Only supports github repositories right now
+  const [_, ghOrg, ghRepoName] = url.match(
+    /https:\/\/github.com\/(\w+)\/([\w_-]+)/
+  );
+  const slug = `${ghOrg}/${ghRepoName}`;
+  const supportsReleases =
+    typeof repo.supportsReleases === "boolean" ? repo.supportsReleases : true;
 
-contents += '| Repository | Type | Package | Released |\n';
-contents += '|------------|:----:|---------|----------|\n';
+  const repository = `[${slug}](https://github.com/${slug})`;
+  const type = repo.packages.map(getTypeBadge).join("<br>");
+  const packages = repo.packages.map(getPackageBadge).join("<br>");
+  const released =
+    !repo.private && supportsReleases
+      ? `![GitHub Release Date](https://img.shields.io/github/release-date/${slug}?color=ffc16b&label=%20)`
+      : "";
 
+  const tableSegments = [repository, type, packages, released];
+  return `| ${tableSegments.join(" | ")} |`;
+};
+
+const colorMap = new Map([
+  ['library', 'yellowgreen'],
+  ['plugin', 'blue'],
+  ['GHA', 'orange'],
+  ['config', 'lightblue'],
+  ['template', 'purple'],
+])
 /**
  * Get the img.shield badge as <type>.
  * @param {name: string, type: 'package' | 'library' } package
  */
 function getTypeBadge(package) {
-  const color = package.type === 'plugin' ? 'blue' : package.type === 'library' ? 'yellowgreen' : 'orange';
-
+  const color = colorMap.get(package.type) ?? 'lightgrey';
   return `![type](https://img.shields.io/badge/%20-${package.type}-${color})`;
 }
 
@@ -29,28 +49,17 @@ function getTypeBadge(package) {
  * @param {name: string, type: 'package' | 'library' } package
  */
 function getPackageBadge(package) {
-  if(package.type !== 'GHA') {
+  if(package.type !== 'GHA' && package.name) {
     return `[![NPM](https://img.shields.io/npm/v/${package.name}.svg?label=${package.name})](https://www.npmjs.com/package/${package.name})`;
   }
 }
 
-for (const repo of list) {
-  const url = repo.url;
-  // Only supports github repositories right now
-  const [_, ghOrg, ghRepoName] = url.match(/https:\/\/github.com\/(\w+)\/([\w_-]+)/);
-  const slug = `${ghOrg}/${ghRepoName}`;
-  const supportsReleases = typeof repo.supportsReleases === 'boolean' ? repo.supportsReleases : true;
+const contents = [
+  HEADER,
+  "| Repository | Type | Package | Released |",
+  "|------------|:----:|---------|----------|",
+  ...repositories.map(repoToTableRow),
+];
 
-  const repository = `[${slug}](https://github.com/${slug})`;
-  const type = repo.packages.map(getTypeBadge).join('<br>');
-  const packages = repo.packages.map(getPackageBadge).join('<br>');
-  const released = !repo.private && supportsReleases ? `![GitHub Release Date](https://img.shields.io/github/release-date/${slug}?color=ffc16b&label=%20)` : '';
-
-  const tableSegments = [repository, type, packages, released];
-  contents += `| ${tableSegments.join(' | ')} |\n`;
-}
-
-contents += '\n';
-
-const newContents = readme.substring(0, repositoryIndex) + contents;
+const newContents = readme.substring(0, repositoryIndex) + contents.join('\n') + '\n';
 fs.writeFileSync('README.md', newContents, '')
